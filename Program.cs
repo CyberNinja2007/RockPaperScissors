54c83c30ff1f84bb7872c5615e95fbecb8cb949a
Georgy <Egor-Nikitin02@yandex.ru>
2021-09-08 21:40:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length % 2 == 0)
+                Console.WriteLine("Error! You need to ");
+        }
+    }
+}
