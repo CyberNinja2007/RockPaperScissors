6b6a2b7a7224c1f8a4dfdd0ae619d2d8feabd337
Georgy <Egor-Nikitin02@yandex.ru>
2021-09-09 00:36:10 +0300

    Updated check on error in Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            if(args.Length % 2 == 0)
-                Console.WriteLine("Error! You need to ");
+            if(args.Length % 2 == 0 || args.Length < 3)
+            {
+                Console.WriteLine(
+                    "Error! You need to input an odd number of args and 3 or more.\nLike this: RockPaperScissors.exe rock paper scissors");
+            }
+            
         }
     }
 }
