68fb7b8d249eb36af6c5cfebc00e3d35878010ce
Georgy <Egor-Nikitin02@yandex.ru>
2021-09-10 11:45:15 +0300

    Filled the key generator with 1 method and 2 fields

--- KeyGenerator.cs
+++ KeyGenerator.cs
@@ -1,7 +1,30 @@
-﻿namespace RockPaperScissors
+﻿using System.Security.Cryptography;
+
+namespace RockPaperScissors
 {
+    /// <summary>
+    /// Represents a secure key generator 
+    /// </summary>
     public class KeyGenerator
     {
+        private RandomNumberGenerator _generator;
+        private byte[] _key;
+
+        public KeyGenerator()
+        {
+            _generator = RandomNumberGenerator.Create();
+            _key = new byte[128];
+        }
+
+        /// <summary>
+        /// Generates the secure 128-bit key
+        /// </summary>
+        /// <returns>The key - an array of 128 bytes</returns>
+        public byte[] GenerateKey()
+        {
+            _generator.GetBytes(_key);
+            return _key;
+        }
         
     }
 }
