19a6046a013034c85de878869a8f5e3b4f3b6fc8
Georgy <Egor-Nikitin02@yandex.ru>
2021-09-10 13:24:48 +0300

    Filled the Encryptor class with 2 methods and 1 field

--- Encryptor.cs
+++ Encryptor.cs
@@ -1,7 +1,43 @@
-﻿namespace RockPaperScissors
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RockPaperScissors
 {
+    /// <summary>
+    /// Represents a class of Encryptor to encode string messages
+    /// </summary>
     public class Encryptor
     {
+        private HMACSHA256 _encryptor;
+
+        /// <summary>
+        /// A constructor for the encryptor
+        /// </summary>
+        /// <param name="key">The key to generate the encryptor</param>
+        public Encryptor(byte[] key)
+        {
+            _encryptor = new HMACSHA256(key);
+        }
+
+        /// <summary>
+        /// Generates the hash of the message
+        /// </summary>
+        /// <param name="message">The message that need to be encrypted</param>
+        /// <returns>The computed hash code in string</returns>
+        public string GenerateHash(string message)
+        {
+            return BitConverter.ToString(_encryptor.ComputeHash(GetBytes(message))).Replace("-", string.Empty).ToLower();
+        }
         
+        /// <summary>
+        /// Gets bytes of the input string
+        /// </summary>
+        /// <param name="message">The string that needs to convert to bytes</param>
+        /// <returns>An array of computed bytes</returns>
+        private byte[] GetBytes(string message)
+        {
+            return new ASCIIEncoding().GetBytes(message);
+        }
     }
 }
