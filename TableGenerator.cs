ffc42e30cddb1e52c2cde0408ac9f7bae6cc20c0
Georgy <Egor-Nikitin02@yandex.ru>
2021-09-12 15:12:49 +0300

    Added all 2 functions and comments

--- TableGenerator.cs
+++ TableGenerator.cs
@@ -1,16 +1,70 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using ConsoleTables;
 
 namespace RockPaperScissors
 {
-    public class TableGenerator
+    /// <summary>
+    /// Represents a class that generates the table with all the results of moves
+    /// </summary>
+    public static class TableGenerator
     {
+        /// <summary>
+        /// Generates the table with results of moves
+        /// </summary>
+        /// <param name="args">The moves you want to be in table</param>
+        /// <returns>The table with the results</returns>
         public static string GenerateTable(string[] args)
         {
-            StringBuilder table = new StringBuilder();
+            ConsoleTable table = new ConsoleTable("USER \\ PC");
+            LinkedList<string> resultOfMoves = GenerateMoves(args.Length);
+
+            table.AddColumn(args);
+
+            for(int i = 0;i < args.Length; i++)
+            {
+                string[] temp = new string[args.Length+1];
+                
+                int j = 0;
+                temp[j] = args[i];
+                
+                foreach (var move in resultOfMoves)
+                {
+                    j++;
+                    temp[j] = move;
+                }
+                
+                table.AddRow(temp);
+
+                LinkedListNode<string> lastMove = resultOfMoves.Last;
+                resultOfMoves.RemoveLast();
+                resultOfMoves.AddFirst(lastMove);
+            }
+
+            return table.ToStringAlternative();
+        }
+
+        /// <summary>
+        /// Generates default moves 
+        /// </summary>
+        /// <param name="length">The length of the moves, you want to generate</param>
+        /// <returns>Returns generated moves</returns>
+        private static LinkedList<string> GenerateMoves(int length)
+        {
+            LinkedList<string> resultOfMoves = new LinkedList<string>();
             
+            resultOfMoves.AddFirst("DRAW");
             
+            for (int i = 1; i <= length/2; i++)
+            {
+                resultOfMoves.AddLast("WIN");
+            }
             
-            return table.ToString();
+            for (int i = length/2+1; i < length; i++)
+            {
+                resultOfMoves.AddLast("LOSE");
+            }
+
+            return resultOfMoves;
         }
     }
 }
